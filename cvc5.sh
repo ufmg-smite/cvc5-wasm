1264a68d3efe79aa06a82d9e41a20f36da3b2ffa
vinciusb <65973642+vinciusb@users.noreply.github.com>
2022-06-30 09:26:59 -0300

    Basic compilation

--- cvc5.sh
+++ cvc5.sh
@@ -6,134 +6,77 @@ BASE_DIR=$(pwd)
 DEP_DIR=$BASE_DIR/deps/
 CVC5_DIR=${DEP_DIR}cvc5/
 EMSDK_DIR=${DEP_DIR}emsdk/
-GMP_DIR=${DEP_DIR}gmp-6.1.2/
-ANTLR_DIR=${DEP_DIR}libantlr3c-3.4/
-LIBPOLY_DIR=${DEP_DIR}libpoly-1383809f2aa5005ef20110fec84b66959518f697/
-INCLUDE_DIR=${DEP_DIR}include/
+LOG_DIR=${BASE_DIR}/logs/
 # Other vars:
-OPTIMIZATION=1
-CORES_TO_COMPILE=4
-LOG_FILE=$BASE_DIR/out.log
->$LOG_FILE
+CORES_TO_COMPILE=6
+BUILD_NAME=production
 
-echo "---------------"
-echo "- Basic deps "
-echo "---------------"
-{
-    sudo apt-get -y -q update
-    mkdir deps
-    # sudo yum check-update
-    # sudo yum groupinstall 'Development Tools'
-    # sudo yum install python3 git cmake lzip
-    sudo apt-get install python3
-
-    cd $DEP_DIR
-    wget http://www.antlr3.org/download/antlr-3.4-complete.jar
-} >> "$LOG_FILE" 2>&1
+cd $LOG_DIR
+rm *
 
 echo ""
-echo "---------------"
+echo "-------------------------------"
 echo "- Downloading deps"
-echo "---------------"
+echo "-------------------------------"
 cd $DEP_DIR
 
 echo "   ---EMSDK";{
+    echo ""
+    echo "Downloading Emscripten:"
+    echo ""
     git clone https://github.com/emscripten-core/emsdk.git
-}>> "$LOG_FILE" 2>&1
-
-echo "   ---GMP";{
-    echo "Cloning into 'GMP'..."
-    wget --quiet -O /tmp/gmp-6.1.2.tar.xz https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz
-    tar -xf /tmp/gmp-6.1.2.tar.xz -C "$DEP_DIR"
-    rm /tmp/gmp-6.1.2.tar.xz
-}>> "$LOG_FILE" 2>&1
-
-echo "   ---ANTLR";{
-    echo "Cloning into 'ANTLR'..."
-    wget --quiet -O /tmp/libantlr3c-3.4.tar.gz http://www.antlr3.org/download/C/libantlr3c-3.4.tar.gz
-    tar -xf /tmp/libantlr3c-3.4.tar.gz -C "$DEP_DIR"
-    rm /tmp/libantlr3c-3.4.tar.gz
-}>> "$LOG_FILE" 2>&1
+}>> "${LOG_DIR}download.log" 2>&1
 
 echo "   ---CVC5";{
-    git clone https://github.com/cvc5/cvc5
-}>> "$LOG_FILE" 2>&1
-
-echo "   ---LIBPOLY";{
-    echo "Cloning into 'libpoly'..."
-    wget --quiet -O /tmp/lib-poly.tar.gz https://github.com/SRI-CSL/libpoly/archive/1383809f2aa5005ef20110fec84b66959518f697.tar.gz
-    tar -xf /tmp/lib-poly.tar.gz -C "$DEP_DIR"
-    rm /tmp/lib-poly.tar.gz
-}>> "$LOG_FILE" 2>&1
+    echo ""
+    echo "Downloading CVC5:"
+    echo ""
+    # Temporarily will be fetched from my personal fork
+    git clone -b wasm https://github.com/vinciusb/cvc5
+}>> "${LOG_DIR}download.log" 2>&1
 
 #Optei por n compilar o cln pq ele é so outra opção pro gmp e não é do meu interesse
 
 echo ""
-echo "---------------"
-echo "- Building deps"
-echo "---------------"
+echo "-------------------------------"
+echo "- Configuring deps"
+echo "-------------------------------"
 
 echo "   ---Emscripten"; {
     echo ""
-    echo "Building Emscripten:"
+    echo "Configuring Emscripten:"
     echo ""
     cd ${EMSDK_DIR}
     git pull
     ./emsdk install latest
     ./emsdk activate latest
     source ./emsdk_env.sh
-} >> "$LOG_FILE" 2>&1
+} >> "${LOG_DIR}config.log" 2>&1
 
-echo '   ---GMP'; {
+echo "   ---CVC5"; {
     echo ""
-    echo "Building GMP:"
+    echo "Configuring CVC5:"
     echo ""
-    cd ${GMP_DIR}
-    emconfigure ./configure --with-pic --disable-assembly --disable-fft --disable-shared --host none --enable-static --enable-cxx --prefix=/home/vinicius/UFMG/IC_ubuntu/cvc5-wasm/deps/teste
-    emmake make -j${CORES_TO_COMPILE}
-} >> "$LOG_FILE" 2>&1
+    
+    CVC5_CONFIGURE_OPTS=(--static --static-binary --no-tracing --no-assertions
+                        --no-debug-symbols --no-unit-testing --name=${BUILD_NAME} --auto-download
+                        )
+                        #  --no-poly)
 
-echo '   ---ANTLR'; {
-    echo ""
-    echo "Building ANTLR:"
-    echo ""
-    cd ${ANTLR_DIR}
-    emconfigure ./configure --with-pic --disable-abiflags --disable-antlrdebug --enable-64bit --disable-shared
-    emmake make -j${CORES_TO_COMPILE}
-} >> "$LOG_FILE" 2>&1
+    CVC5_CONFIGURE_ENV=(CXXFLAGS="-I${GMP_DIR} -I${ANTLR_DIR}"
+                        LDFLAGS="-L${GMP_DIR}.libs -L${ANTLR_DIR}.libs")
 
-echo "   ---LIBPOLY";{
-    echo ""
-    echo "Building libpoly:"
-    echo ""
-    cd ${LIBPOLY_DIR}
-    emmake make -DCMAKE_BUILD_TYPE=Release 
-                -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
-                -DCMAKE_TOOLCHAIN_FILE=
-                -DLIBPOLY_BUILD_PYTHON_API=OFF
-                -DLIBPOLY_BUILD_STATIC=ON
-                -DLIBPOLY_BUILD_STATIC_PIC=ON
-                -DGMP_INCLUDE_DIR=${GMP_DIR}
-                -DGMP_LIBRARY=${GMP_DIR}.libs/libgmp.a
-                -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=TRUE
-
-}>> "$LOG_FILE" 2>&1
-
-CVC5_CONFIGURE_OPTS=(--static --static-binary --no-tracing --no-assertions
-                     --no-debug-symbols --no-unit-testing --name=production --auto-download)
-                    #  --no-poly)
-
-CVC5_CONFIGURE_ENV=(
-                    # ANTLR="${DEP_DIR}antlr-3.4-complete.jar"
-                    CXXFLAGS="-I${GMP_DIR} -I${ANTLR_DIR}"
-                    LDFLAGS="-L${GMP_DIR}.libs -L${ANTLR_DIR}.libs")
-
-echo '   ---CVC5'; {
     cd ${CVC5_DIR}
     env "${CVC5_CONFIGURE_ENV[@]}" emconfigure ./configure.sh "${CVC5_CONFIGURE_OPTS[@]}"
-    cd ./production/
-    emmake make -j${CORES_TO_COMPILE}
-} >> "$LOG_FILE" 2>&1
+} >> "${LOG_DIR}config.log" 2>&1
 
-# emconfigure /home/vinicius/UFMG/IC_ubuntu/cvc5-wasm/deps/cvc5/production/deps/src/GMP-EP/configure --disable-shared --enable-static --prefix=/home/vinicius/UFMG/IC_ubuntu/cvc5-wasm/deps/cvc5/production/deps --with-pic --enable-cxx
-# emconfigure ./configure --disable-shared --enable-static --prefix=/home/vinicius/UFMG/IC_ubuntu/cvc5-wasm/deps/cvc5/production/deps --with-pic --enable-cxx
+
+echo ""
+echo "-------------------------------"
+echo "- Building CVC5"
+echo "-------------------------------"
+
+echo '   ---CVC5'; {
+    cd ./${BUILD_NAME}
+    emmake make -j${CORES_TO_COMPILE}
+} >> "${LOG_DIR}build.log" 2>&1
